98e2c7be5fb657c3feca376e1365a77fc773751c
Tanmay Rathod <tanmay.3mystic@gmail.com>
2020-12-09 18:17:07 +0530

    hgfh

--- src/index.js
+++ src/index.js
@@ -26,13 +26,50 @@ app.get("/api/student" , (req,res)=>{
 
 app.get("/api/student/:id" , (req,res)=>{
     const id = req.params.id;
-    const student = studentArray.find((stud)=>stud.id==id)
+    const student = studentArray.find((stud)=>stud.id===Number(id))
     if(student)
     res.send(student);
     else 
-    res.status(404).send("bad req");
+    res.sendStatus(404);
 })
 
+const isValid = (data)=>data===null || data===undefined ; 
+
+app.post("/api/student" , (req,res)=>{
+    let body = req.body
+    const {name , currentClass , division} = body;
+    if(isValid(name) || isValid(currentClass) || isValid(division))
+    res.sendStatus(404);
+    else{
+        body.id = studentArray.length+1;
+        body.currentClass= Number(body.currentClass)
+        studentArray.push(body);
+        res.send({id: body.id});
+    }
+})
+
+app.put("/api/student/:id",(req,res)=>{
+    const update = req.body;
+    const id = req.params.id;
+    const matchedIdx = studentArray.findIndex((stud)=>stud.id===Number(id));
+    if(matchedIdx===-1) res.sendStatus(400);
+    let {name , currentClass , division}=update;
+    if(isValid(name) || isValid(currentClass) || isValid(division)) res.sendStatus(400);
+    else {
+        !isValid(name)?studentArray[matchedIdx].name=name:null;
+        !isValid(currentClass)?studentArray[matchedIdx].currentClass=Number(currentClass):null;
+        !isValid(division)?studentArray[matchedIdx].division=division:null;
+        res.sendStatus(200);
+    }
+})
+
+app.delete("/api/student/:id" , (req,res)=>{
+    const id = req.params.id;
+    const matchedIdx = studentArray.findIndex((stud)=>stud.id==id);
+    if(matchedIdx==-1) res.sendStatus(404);
+    studentArray.splice(matchedIdx , 1);
+    res.sendStatus(200);
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
