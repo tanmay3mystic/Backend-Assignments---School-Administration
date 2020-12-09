dec661db2e09734a0a2070460d2595897f19045b
Tanmay Rathod <tanmay.3mystic@gmail.com>
2020-12-09 11:05:08 +0530

    dg

--- src/index.js
+++ src/index.js
@@ -12,8 +12,27 @@ app.use(bodyParser.json())
 const studentArray = require("./initialData.js");
 
 app.get("/api/student" , (req,res)=>{
-    res.send(JSON.stringify(studentArray));
+    res.send(studentArray);
 })
+// app.get("/api/student/:id" , (req,res)=>{
+//     const id=req.params.id
+//     console.log(typeof studentArray[id])
+//     if(id==parseInt(id,10) && studentArray[id-1])
+//       res.send(studentArray[id-1]);
+//     else
+//       res.status(404).send("bad req");
+   
+// })
+
+app.get("/api/student/:id" , (req,res)=>{
+    const id = req.params.id;
+    const student = studentArray.find((stud)=>stud.id==id)
+    if(student)
+    res.send(student);
+    else 
+    res.status(404).send("bad req");
+})
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
